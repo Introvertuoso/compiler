d8459babd81a9a33687e227e73f4b2c909d14e79
Jawad <48954771+Introvertuoso@users.noreply.github.com>
2019-05-14 07:29:16 +0300

    message

--- Compiler/src/GUI.java
+++ Compiler/src/GUI.java
@@ -33,8 +33,8 @@ public class GUI extends Application {
         window.setScene(submitScene);
         window.show();
     }
-    private VBox submission()
-    {
+
+    private VBox submission() {
 
         Label label = new Label("Enter String to be checked: ");
         TextField text = new TextField();
@@ -86,7 +86,8 @@ public class GUI extends Application {
         v.getChildren().addAll(label,h);
         return v;
     }
-        public static ArrayList<String> readFile(String filename){
+
+    public static ArrayList<String> readFile(String filename){
         ArrayList<String> result = new ArrayList<>();
         String regex = "";
         try{
